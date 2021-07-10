ada4475e5f16c8ccfface79ad8131e4ec1bccc76
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-07-10 15:14:16 +0100

    added the opening hours and about restaurant page to homepage

--- src/index.js
+++ src/index.js
@@ -13,11 +13,28 @@ function component() {
       <main>
           <section class="menu">
               <ul class="menu-list">
-                  <li><button>Menu</button></li>
-                  <li><button>Contact us</button></li>
-                  <li><button>About Us </button></li>
+                  <li class="list"><button>Menu</button></li>
+                  <li class="list"><button>Contact us</button></li>
+                  <li class="list"><button>About Us </button></li>
               </ul>
           </section>
+      <section class="about">
+        <h6>About TakeMe Away Restaurant</h6>
+        <p>Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.</p>
+    </section>
+    <section class="hours">
+        <h6>Opening Hours</h6>
+        <ul>
+          <li>Sunday: 8am - 8pm</li>
+          <li>Monday: 6am - 6pm</li>
+          <li>Tuesday: 6am - 6pm </li>
+          <li>Wednesday: 6am - 6pm</li>
+          <li>Thursday: 6am - 10pm</li>
+          <li>Friday: 6am - 10pm</li>
+          <li>Saturday: 8am - 10pm</li>
+        </ul>
+
+    </section>
       </main>`;
 
    return element;
