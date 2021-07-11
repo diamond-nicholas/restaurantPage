e32d1f86dce89c2ce5bc1571f45ee46a4d50c02e
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-07-11 03:45:29 +0100

    add the main logic to index.js

--- src/index.js
+++ src/index.js
@@ -11,9 +11,29 @@ const myBg =  new Image();
 myBg.src = Bg
 
 
+const content = document.getElementById("content");
 myNav();
 home();
 
+document.addEventListener('click', (event) => {
+  const value = event.target.innerText;
+
+  if (value === 'Home') {
+    content.innerHTML = '';
+    myNav();
+    home();
+  }
+  if (value === 'Menu') {
+    content.innerHTML = '';
+    myNav();
+    menuList();
+  }
+  if (value === "Contact") {
+    content.innerHTML = "";
+    myNav();
+    contact();
+  }
+});
 // menuList();
 // contact()
 // const main = document.querySelector('#content')
